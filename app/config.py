2ac760e1da5c5426c4671793cbf90cc1f784fa1f
risto <risto.kowaczewski@gmail.com>
2024-01-15 10:36:53 +0100

    feat(config): remove unnecessary 'production' variable
    Remove the unnecessary 'production' variable from the Config class constructor.

--- app/config.py
+++ app/config.py
@@ -6,7 +6,6 @@ from tracardi.service.utils.environment import get_env_as_int
 
 class Config:
     def __init__(self, env):
-        self.production = env.get('PRODUCTION', 'no') == 'yes'
         self.tenant = env.get('TENANT', tracardi.version.name)
         self.mode = env.get('MODE', 'job')
         self.pause = get_env_as_int('PAUSE', 60)
