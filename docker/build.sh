fad1268e51c2b1d3c1771c5e302194030c5246e1
risto <risto.kowaczewski@gmail.com>
2024-01-16 18:41:08 +0100

    refactor: update Dockerfile and build.sh for com_tracardi prerequisites
    - Update Dockerfile to copy the com_tracardi folder to the app directory
    - Update build.sh to clone the com-tracardi repository and build the Docker image
    - Remove unnecessary files after the build process

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,7 @@
-#docker build ../ --rm --progress=plain -f Dockerfile -t tracardi/apm:0.8.2-rc3
-docker build ../ --rm --no-cache --progress=plain -f Dockerfile -t tracardi/apm:0.8.2-rc3
+rm -rf prerequisites
+git clone https://github.com/Tracardi/com-tracardi -b 0.8.2-dev prerequisites
+
+docker build ../ --rm --no-cache --progress=plain -f Dockerfile -t tracardi/apm:0.8.2-rc5
+docker push tracardi/apm:0.8.2-rc5
+
+rm -rf prerequisites
