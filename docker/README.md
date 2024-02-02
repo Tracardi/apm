86a57f7f7764020758f99bea4adb572634350714
risto <risto.kowaczewski@gmail.com>
2024-02-02 11:35:38 +0100

    New version 0.8.2

--- docker/README.md
+++ docker/README.md
@@ -4,4 +4,4 @@ docker run \
 -e PULSAR_HOST=pulsar://192.168.1.110:6650 \
 -e MODE=worker \
 -e PAUSE=5 \
-tracardi/apm:0.8.2-rc4
+tracardi/apm:0.8.2.1
--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 rm -rf prerequisites
-git clone https://github.com/Tracardi/com-tracardi -b 0.8.2-dev prerequisites
+git clone https://github.com/Tracardi/com-tracardi -b 0.8.2 prerequisites
 
-docker build ../ --rm --no-cache --progress=plain -f Dockerfile -t tracardi/apm:0.8.2-rc5
-docker push tracardi/apm:0.8.2-rc5
+docker build ../ --rm --no-cache --progress=plain -f Dockerfile -t tracardi/apm:0.8.2.1
+docker push tracardi/apm:0.8.2.1
 
 rm -rf prerequisites
